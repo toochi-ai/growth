1a4ae19bbf785770dffa35de096f7949657f06e5
Mikeilov Evstafiy <mikeilov.ea@gmail.com>
2025-07-13 19:13:24 +0300

    добавил, на каждый апдейт присылает кота

--- growth.py
+++ growth.py
@@ -2,26 +2,31 @@ import requests
 import time
 
 API_URL = 'https://api.telegram.org/bot'
-BOT_TOKEN = '*****'
-TEXT = 'AMAZING!'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = '8121082222:AAEcKn7P9V4D8SwgJHosDUWaRTWD9djiS1A'
+ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
 MAX_COUNTER = 100
 
 offset = -2
 counter = 0
-chat_id: int
+cat_response: requests.Response
+cat_link: str
 
 while counter < MAX_COUNTER:
 
     print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
-
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-    print(updates)
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
 
     time.sleep(1)
     counter += 1
