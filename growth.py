202c78e48c45b1a803dfbe28eaa86ee44cda2b9c
Mikeilov Evstafiy <mikeilov.ea@gmail.com>
2025-07-13 22:13:26 +0300

    так может выглядеть polling со стороны бэкэнда нашего бота

--- growth.py
+++ growth.py
@@ -2,31 +2,25 @@ import requests
 import time
 
 API_URL = 'https://api.telegram.org/bot'
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
-BOT_TOKEN = '8121082222:AAEcKn7P9V4D8SwgJHosDUWaRTWD9djiS1A'
-ERROR_TEXT = 'Здесь должна была быть картинка с котиком :('
-MAX_COUNTER = 100
+BOT_TOKEN = '*****************'
 
 offset = -2
-counter = 0
-cat_response: requests.Response
-cat_link: str
+updates: dict
 
-while counter < MAX_COUNTER:
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+def do_something() -> None:
+    print('Был апдейт')
+
+
+while True:
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+            do_something()
 
-    time.sleep(1)
-    counter += 1
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
