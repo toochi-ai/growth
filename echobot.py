b828e22f7074b3975e36eb8fbcddefeb21c5a3f5
Mikeilov Evstafiy <mikeilov.ea@gmail.com>
2025-07-17 00:37:03 +0300

    Полноценный Эхо-бот

--- echobot.py
+++ echobot.py
@@ -1,9 +1,9 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
 
-BOT_TOKEN = '****************************'
+BOT_TOKEN = '8121082222:AAEcKn7P9V4D8SwgJHosDUWaRTWD9djiS1A'
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
@@ -11,11 +11,13 @@ dp = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
@@ -23,34 +25,17 @@ async def process_help_command(message: Message):
     )
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
-
-
-# Этот хэндлер будет срабатывать на отправку боту фото
-async def send_photo_echo(message: Message):
-    print(message)
-    await message.reply_photo(message.photo[0].file_id)
-
-
-# Этот хэндлер будет срабатывать на отправку боту стикеров
-async def send_sticker_echo(message: Message):
-    await message.reply_sticker(message.sticker.file_id)
-
-
-# Этот хэндлер будет срабатывать на отправку боту voice
-async def send_voice_echo(message: Message):
-    await message.reply_voice(message.voice.file_id)
-
-
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(send_sticker_echo, F.sticker)
-dp.message.register(send_voice_echo, F.voice)
-dp.message.register(send_echo)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
 
 if __name__ == '__main__':
